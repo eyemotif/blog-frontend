d2f76a9a34ff1b3467b8eaed3caffd79628d250a
eyemotif <yepicness@gmail.com>
2024-01-27 17:58:42 -0500

    Fix post sort order

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,38 @@
 <script setup lang="ts">
 import { req, type Post, postText } from '@/api'
 import PostComponent from '@/components/PostComponent.vue'
+import { computed, ref, type Ref } from 'vue'
 import { useCookies } from 'vue3-cookies'
 
 const { cookies } = useCookies()
 const latestPostMetas: Post[] = JSON.parse(await req('/post/latest/10/0'))
-let latestPosts: [Post, string][] = []
+const sortedLatestPosts = computed(() => {
+    const latestPostsInner = [...latestPosts.value]
+    latestPostsInner.sort((a, b) => {
+        const aTimestamp = new Date(a[0].timestamp).getTime()
+        const bTimestamp = new Date(b[0].timestamp).getTime()
+        return -(aTimestamp - bTimestamp) // reverse order
+    })
+    return latestPostsInner
+})
 
-await Promise.all(latestPostMetas.map(async post => {
+let latestPosts: Ref<[Post, string][]> = ref([])
+latestPostMetas.map(async post => {
     try {
         const text = await postText(post.id, cookies.get('frithblog-session'))
         if (text !== null)
-            latestPosts.push([post, text])
+            latestPosts.value.push([post, text])
     }
     catch (err) {
         console.log(`error getting post ${post.id}: ${err}`)
     }
-}))
+})
 </script>
 
 <template>
     <header id="title">latest posts</header>
     <hr>
-    <section v-for="[post, text] in latestPosts">
+    <section v-for="[post, text] in sortedLatestPosts">
         <PostComponent :post="post" :text="text" />
         <hr>
     </section>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -3,25 +3,34 @@ import { req, type Post, type User, postText, } from '@/api'
 import { useRoute } from 'vue-router'
 import PostComponent from '@/components/PostComponent.vue'
 import { useCookies } from 'vue3-cookies'
+import { computed, ref, type Ref } from 'vue'
 
 const route = useRoute()
 const { cookies } = useCookies()
 const user: User = JSON.parse(await req(`/user/${route.params.username}`))
+const sortedUserPosts = computed(() => {
+    const userPostsInner = [...userPosts.value]
+    userPostsInner.sort((a, b) => {
+        const aTimestamp = new Date(a[0].timestamp).getTime()
+        const bTimestamp = new Date(b[0].timestamp).getTime()
+        return -(aTimestamp - bTimestamp) // reverse order
+    })
+    return userPostsInner
+})
 
-let userPosts: [Post, string][] = []
-
-await Promise.all(user.posts.reverse().map(async postID => {
+let userPosts: Ref<[Post, string][]> = ref([])
+user.posts.reverse().map(async postID => {
     try {
         const meta: Post = JSON.parse(await req(`/post/${postID}/meta`))
         const text = await postText(postID, cookies.get('frithblog-session'))
 
         if (text !== null)
-            userPosts.push([meta, text])
+            userPosts.value.push([meta, text])
     }
     catch (err) {
         console.log(`error getting post ${postID}: ${err}`)
     }
-}))
+})
 
 </script>
 
@@ -29,7 +38,7 @@ await Promise.all(user.posts.reverse().map(async postID => {
     <header id="title" @click="() => $router.push('/')">
         {{ user.name }}'s posts</header>
     <hr>
-    <section v-for="[post, text] in userPosts">
+    <section v-for="[post, text] in sortedUserPosts">
         <PostComponent :post="post" :text="text" />
         <hr>
     </section>
